e840030eb260efd17ffa5a9eb73b9a22746b2c2f
paul <paul.brechen@gmail.com>
2025-04-23 16:14:47 +0200

    ajout noising final + base d'image xo et xb

--- src/model/base/Img.java
+++ src/model/base/Img.java
@@ -1,25 +1,82 @@
 package model.base;
 
-import javafx.scene.image.Image;
+import java.awt.image.BufferedImage;
+import java.awt.image.WritableRaster;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 public class Img {
 	
+	private int largeur;
+	private int hauteur;
 	private double[][] pixels;
-	private Image image;
 	
-	public Img(double[][] pixels) {
-		this.pixels = pixels;
+	public Img(String filename) throws IOException {
+		BufferedImage image = ImageIO.read(new File(filename));
+		this.hauteur = image.getHeight();
+		this.largeur = image.getWidth();
+		this.pixels = new double[hauteur][largeur];
+		
+		
+		WritableRaster raster = image.getRaster();
+		for(int i = 0; i < hauteur; i++) {
+			for(int j = 0; j < largeur; j++) {
+				pixels[i][j] = raster.getSampleDouble(j, i, 0);
+			}	
+		}
+	}
+	
+	public Img(double generatedPixels[][]){
+	  this.hauteur = generatedPixels.length;
+      this.largeur = generatedPixels[0].length;
+	  this.pixels = new double[hauteur][largeur];
+
+		
+	  for (int i = 0; i <  this.hauteur; i++) {
+            for (int j = 0; j <  this.largeur; j++) {
+                this.pixels[i][j] = generatedPixels[i][j];
+            }
+        }
 	}
 	
 	public int getHauteur() {
-		return (int) image.getHeight();
+		return this.hauteur;
 	}
 	
 	public int getLargeur() {
-		return (int) image.getWidth();
+		return this.largeur;
+	}
+	
+	public double[][] getPixels() {
+		return this.pixels;
 	}
 	
 	public double getPixel(int x, int y) {
 		return this.pixels[x][y];
 	}
+	
+	
+	public void saveImg(String filename) throws IOException {
+		BufferedImage image = new BufferedImage(this.largeur, this.hauteur, BufferedImage.TYPE_BYTE_GRAY);
+
+
+		
+		 for (int i = 0; i <  this.hauteur; i++) {
+	            for (int j = 0; j <  this.largeur; j++) {
+	            	int pixelValue = (int) Math.min(255, Math.max(0, Math.round(this.getPixel(i, j))));
+	            	image.getRaster().setSample(j, i , 0, pixelValue);
+	            }
+	        }
+		
+		
+		
+		String format = filename.substring(filename.lastIndexOf('.') + 1);
+        ImageIO.write(image, format, new File(filename));
+	}
+	
+	
+	
+	
 }
--- src/service/bruit/BruiteurImage.java
+++ src/service/bruit/BruiteurImage.java
@@ -1,21 +1,51 @@
 package service.bruit;
 
+import java.io.IOException;
 import java.util.Random;
-
-import javafx.scene.image.Image;
 import model.base.Img;
 
 public class BruiteurImage {
 		
-	public static void main(String[] args) {
-		double sigma = 10;
-		Image x0 = new Image("file:data/x0/lena.png");
-		System.out.println(x0.getPixelReader().getPixelFormat());
-//		Img xB = noising(x0,sigma);
+	public static void main(String[] args) {		
+		String[] imageNames = {
+			    "ali_gray.jpg",
+			    "crocodilo_gray.jpg",
+			    "darkvador_gray.jpg",
+			    "gekko_gray.jpg",
+			    "harrypotter_gray.png",
+			    "leclerc_gray.png",
+			    "lena_gray.png",
+			    "mbappe_gray.jpg",
+			    "moto_gray.jpeg",
+			    "nyancat_gray.png",
+			    "steve_gray.jpg",
+			    "wemby_gray.png"
+			};
+		
+		for(String imageName : imageNames) {
+			Img x0 = null;
+			try {
+				x0 = new Img("data/x0/"+imageName);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}			
+			for(double sigma=10; sigma < 40; sigma+=10) {
+				Img xB = noising(x0,sigma);
+								
+				try {
+					xB.saveImg("data/xB/"+(int) sigma+"/"+imageName);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			System.out.println("Traitement de l'image "+ imageName + " terminé");
+		}
+			
+			
 		return;
 	}
 	
-	public Img noising(Img x0, double sigma) {
+	public static Img noising(Img x0, double sigma) {
 		Random r = new Random();
 		double xB_pixels[][] = new double[x0.getHauteur()][x0.getLargeur()];
 		for (int i = 0; i<x0.getHauteur();i++) {
@@ -24,6 +54,8 @@ public class BruiteurImage {
 			}
 		}
 		Img xB = new Img(xB_pixels);
+		
+		
 		return xB;
 	}
 
