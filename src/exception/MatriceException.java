3b6d778a1269777e89cf24af505f040a3a41abe5
Emma <emmalesbarreres@gmail.com>
2025-05-15 11:30:59 +0200

    diagonal + doxygen debut

--- src/exception/MatriceException.java
+++ src/exception/MatriceException.java
@@ -1,6 +1,26 @@
+/**
+ * @file MatriceException.java
+ * @brief Définition de l'exception spécifique pour les erreurs liées aux matrices.
+ * 
+ * Cette exception est levée lorsqu'une opération sur une matrice rencontre une erreur.
+ */
+
 package exception;
 
+/**
+ * @class MatriceException
+ * @brief Exception spécifique pour les erreurs liées aux matrices.
+ * @author Emma
+ * @extends RuntimeException
+ * Il s'agit d'une exception non vérifiée.
+ */
 public class MatriceException extends RuntimeException {
+	
+    /**
+     * @brief Constructeur de l'exception MatriceException.
+     * @author Emma
+     * @param msg Le message décrivant l'erreur rencontrée.
+     */
 	public MatriceException(String msg) {
 		super(msg);
 	}
--- src/model/acp/ResultatACP.java
+++ src/model/acp/ResultatACP.java
@@ -1,27 +1,65 @@
+/**
+ * @file ResultatACP.java
+ * @brief Classe représentant le résultat d'une Analyse en Composantes Principales (ACP).
+ */
+
 package model.acp;
+
 import model.base.Matrice;
 import model.base.Vecteur;
 
+/**
+ * @class ResultatACP
+ * @brief Contient les résultats d'une Analyse en Composantes Principales (ACP).
+ * @author Emma
+ */
 public class ResultatACP {
-	private double[] valeursPropres;
-	private Matrice vecteursPropres;
-	private Vecteur vecteurMoyen;
-	
-	public ResultatACP(double[] valeursPropres, Matrice vecteursPropres, Vecteur vecteurMoyen) {
-		this.valeursPropres = valeursPropres;
-		this.vecteurMoyen = vecteurMoyen;
-		this.vecteursPropres = vecteursPropres;
-	}
 
-	public double[] getValeursPropres() {
-		return valeursPropres;
-	}
+	//les valeurs propres (inerties)
+    private double[] valeursPropres;
+    //les vecteurs propres (axes principaux)
+    private Matrice vecteursPropres;
+    //le vecteur moyen des données initiales
+    private Vecteur vecteurMoyen;
 
-	public Matrice getVecteursPropres() {
-		return vecteursPropres;
-	}
+    /**
+     * @brief Constructeur du résultat d'ACP.
+     * @author Emma
+     * @param valeursPropres Tableau des valeurs propres (inerties) de l'ACP.
+     * @param vecteursPropres Matrice des vecteurs propres (axes principaux).
+     * @param vecteurMoyen Vecteur moyen des données initiales.
+     */
+    public ResultatACP(double[] valeursPropres, Matrice vecteursPropres, Vecteur vecteurMoyen) {
+        this.valeursPropres = valeursPropres;
+        this.vecteurMoyen = vecteurMoyen;
+        this.vecteursPropres = vecteursPropres;
+    }
 
-	public Vecteur getVecteurMoyen() {
-		return vecteurMoyen;
-	}
+    /**
+     * @brief Accède aux valeurs propres du résultat d'ACP.
+     * @author Emma
+     * @return Tableau des valeurs propres.
+     */
+    public double[] getValeursPropres() {
+        return valeursPropres;
+    }
+
+    /**
+     * @brief Accède aux vecteurs propres du résultat d'ACP.
+     * @author Emma
+     * @return Matrice des vecteurs propres.
+     */
+    public Matrice getVecteursPropres() {
+        return vecteursPropres;
+    }
+
+    /**
+     * @brief Accède au vecteur moyen des données initiales.
+     * @author Emma
+     * @return Le vecteur moyen.
+     */
+    public Vecteur getVecteurMoyen() {
+        return vecteurMoyen;
+    }
 }
+
